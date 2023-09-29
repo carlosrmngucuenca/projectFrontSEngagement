803015b857e7e1756ca9dd836fc9d51e512d0b5a
Felipe Mendieta <felipe.mendieta98@ucuenca.edu.ec>
2023-09-29 00:29:58 -0500

    Update buttons activities
    update and test socket connection. All runs.
    Only there is a problem, when navigating to other component from Home, the socket has desconected and reconected, losting the session and have a new session.

--- src/app/student/pages/home/home.component.ts
+++ src/app/student/pages/home/home.component.ts
@@ -8,15 +8,20 @@ import { socketService } from 'src/app/services/socket.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit{
+  roomCode= "MRA123";
+  data={roomCode:this.roomCode};
   constructor(private socketService: socketService,) {}
   ngOnInit(): void {
-
+    this.socketService.onSuccess()
+      .subscribe((data) => {
+        console.log('success', data);
+      });
   }
   buttonActivitySleep() {
-    //this.socketService.emitEvent('saveActivitySleep', this.data);
+    this.socketService.emit('saveActivitySleep', this.data);
   }
   buttonActivityIdontGetIt() {
-    //this.socketService.emitEvent('saveActivityIdontGetIt', this.data);
+    this.socketService.emit('saveActivityIdontGetIt', this.data);
   }
   ngOnDestroy() {
     this.socketService.disconnect();
