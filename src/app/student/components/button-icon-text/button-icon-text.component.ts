a2be84aca09236ae2e1f2ddb782143a82bbda86b
Felipe Mendieta <felipe.mendieta98@ucuenca.edu.ec>
2023-10-10 20:18:46 -0500

    Fix error for change name component

--- src/app/student/components/button-icon-text/button-icon-text.component.ts
+++ src/app/student/components/button-icon-text/button-icon-text.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'student-button-image-text',
+  selector: 'student-button-icon-text',
   templateUrl: './button-icon-text.component.html',
   styleUrls: ['./button-icon-text.component.css']
 })
