563ebec925a2407395394265bd803fe43592ffe3
carlosrmngucuenca <carlos.munozg@ucuenca.edu.ec>
2023-10-26 16:38:30 -0500

    guards

--- src/app/student/components/single-option-question/single-option-question.component.ts
+++ src/app/student/components/single-option-question/single-option-question.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
-import { Question } from 'src/app/interfaces/poll.interface';
+import { Answer, Question } from 'src/app/interfaces/poll.interface';
 
 @Component({
   selector: 'app-single-option-question',
@@ -11,6 +11,8 @@ export class SingleOptionQuestionComponent implements OnInit {
   @Input() question!: Question; // Replace 'any' with the appropriate question interface
   @Input() form!: FormGroup;
   myOption = this.formBuilder.group({});
+  previousAnswer = '';
+  answerOption = '';
   constructor(private formBuilder: FormBuilder) {
     this.form = new FormGroup({
       answer: new FormControl(null),
@@ -23,13 +25,22 @@ export class SingleOptionQuestionComponent implements OnInit {
   buildForm() {
     let idQuestion = this.question._id;
 
-    this.form.addControl(idQuestion, this.formBuilder.control(false));
+    this.form.addControl(this.question._id, this.myOption);
   }
   get answers() {
     return this.question.answers;
   }
 
-  saveOption(option: string | undefined) {
-    console.log('hola', option);
+  saveOption(answerId: string, answer: string) {
+    const controlCount = Object.keys(this.myOption.controls).length;
+
+    if (controlCount == 1) {
+      this.myOption.removeControl(this.previousAnswer);
+      this.myOption.addControl(answerId, this.formBuilder.control(answer));
+      this.previousAnswer = answerId;
+    } else {
+      this.myOption.addControl(answerId, this.formBuilder.control(answer));
+      this.previousAnswer = answerId;
+    }
   }
 }
