1f9be89fffbfb34973eb02a6049d1d815948b492
Felipe Mendieta <felipe.mendieta98@ucuenca.edu.ec>
2023-11-26 23:54:59 -0500

    Badge page
    Create a badge page. When the user get 100% in the progress bar a image badge appears.

--- src/app/modules/student/pages/my-badge/my-badge.component.ts
+++ src/app/modules/student/pages/my-badge/my-badge.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
+import { ProgressBarService } from 'src/app/services/progress-bar.service';
 
 @Component({
   selector: 'app-my-badge',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-badge.component.css']
 })
 export class MyBadgeComponent {
+  percentProgress = this.progressBarService.getProgress$();
+  titleButton = 'Reclamar mi premio: ' + this.percentProgress + '%';
+  isShowedBadge = this.percentProgress >= 90 ? true : false;
+  constructor(
+    // Add any needed services here
+    private progressBarService: ProgressBarService,
+  ) {
+    console.log(progressBarService.getProgress$());
+  }
+  badges = [
+    { title: 'Reclamar mi premio' },
+    { title: 'Badge 2' },
+    { title: 'Badge 3' },
+    // Add more badges as needed
+  ];
 
 }
